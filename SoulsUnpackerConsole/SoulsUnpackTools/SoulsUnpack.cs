6bc84a2864b0d2f93df2727e8d33d22031dc7840
zelenák lazarus012 <zelenakandras2003@gmail.com>
2022-04-27 15:23:04 +0200

    Add basic project files (console version)

---
+++ SoulsUnpackerConsole/SoulsUnpackTools/SoulsUnpack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SoulsFormats;
+
+namespace SoulsUnpackTools {
+    public static class SoulsUnpack {
+        public static void SayHello() {
+            Console.WriteLine("Hello!");
+            SaySouls();
+        }
+
+        internal static void SaySouls() {
+            BND3 bnd3 = new BND3();
+            Console.WriteLine(bnd3.Version);
+        }
+    }
+}
---
+++ SoulsUnpackerConsole/SoulsUnpackerConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using SoulsUnpackTools;
+
+namespace SoulsUnpackerConsole {
+    internal class Program {
+        static void Main(string[] args) {
+            SoulsUnpack.SayHello();
+        }
+    }
+}
