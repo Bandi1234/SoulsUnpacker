e3a2538e15d3397aa37d3aef5e25aaa968ccad94
zelenák lazarus012 <zelenakandras2003@gmail.com>
2022-10-08 12:08:48 +0200

    Add better console loading bars

--- SoulsUnpacker/SoulsUnpackerConsole/ConsoleLoadingBar.cs
+++ SoulsUnpacker/SoulsUnpackerConsole/ConsoleLoadingBar.cs
@@ -13,12 +13,16 @@ namespace SoulsUnpackerConsole {
         private int maxSigns = 0;
 
         private int consoleWidth = 0;
+        private int percentage1 = 0;
+        private int percentage2 = 0;
+        private int percentage3 = 0;
+
         public ConsoleLoadingBar(string title, int currentProgress, int totalProgress) { 
             this.title = title;
             this.currentProgress = currentProgress;
             this.totalProgress = totalProgress;
             this.consoleWidth = Console.WindowWidth - 2;
-            this.maxSigns = consoleWidth - 5;
+            this.maxSigns = consoleWidth - 13;
 
             FirstDraw();
         }
@@ -30,21 +34,54 @@ namespace SoulsUnpackerConsole {
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine();
-            Console.Write(" <");
-            Console.SetCursorPosition(consoleWidth - 2, Console.CursorTop);
-            Console.Write("> ");
+            string firstDraw = " [";
+            for (int i = 0; i < maxSigns; i++) {
+                firstDraw += ".";
+            }
+            firstDraw += "] { 00.0% } ";
+            Console.Write(firstDraw);
         }
 
         public void Update(int newProgress) {
             int nextSigns = (int)Math.Round((float)newProgress / (float)totalProgress * maxSigns);
             if (nextSigns > currentSigns) {
-                Console.SetCursorPosition(2 + currentSigns, Console.CursorTop);
-                for (int i = 0; i < nextSigns - currentSigns; i++) {
+                if (currentSigns > 0) {
+                    Console.SetCursorPosition(1 + currentSigns, Console.CursorTop);
                     Console.Write("=");
                 }
+                Console.SetCursorPosition(2 + currentSigns, Console.CursorTop);
+                for (int i = 0; i < nextSigns - currentSigns - 1; i++) {
+                    Console.Write("=");
+                }
+                Console.Write(">");
             }
 
             currentSigns = nextSigns;
+
+            float newNum = (float)Math.Round((float)newProgress / (float)totalProgress, 3) * 100;
+            if (newNum > 99.9f) {
+                newNum = 99.9f;
+            }
+            int newPercentage1 = int.Parse(newNum.ToString("00.0")[0] + "");
+            int newPercentage2 = int.Parse(newNum.ToString("00.0")[1] + "");
+            int newPercentage3 = int.Parse(newNum.ToString("00.0")[3] + "");
+            if (newPercentage1 != percentage1) {
+                Console.SetCursorPosition(6 + maxSigns, Console.CursorTop);
+                Console.Write(newPercentage1);
+                percentage1 = newPercentage1;
+            }
+            if (newPercentage2 != percentage2) {
+                Console.SetCursorPosition(7 + maxSigns, Console.CursorTop);
+                Console.Write(newPercentage2);
+                percentage2 = newPercentage2;
+            }
+            if (newPercentage3 != percentage3) {
+                Console.SetCursorPosition(9 + maxSigns, Console.CursorTop);
+                Console.Write(newPercentage3);
+                percentage3 = newPercentage3;
+            }
+            
+
             currentProgress = newProgress;
         } 
     }
